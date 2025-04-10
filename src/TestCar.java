593233fe59e399c405a520e4fb3fd3968e725fc8
Jem <jemalynebacuado942@gmail.com>
2025-04-11 01:14:13 +0800

    Delete src directory

--- src/TestCar.java
+++
@@ -1,9 +0,0 @@
-public class TestCar {
-    public static void main(String[] args) {
-        Car car1 = new Car("Camry");
-        System.out.println(car1.toString());
-        
-        Car car2 = new Car("Veloz");
-        System.out.println(car2.toString());
-         }
-}
